f836609f539014e001bc6e8e615383bb7821312d
email <shakti.agrawal@publicissapient.com>
2020-03-25 20:44:24 +0530

    Clone Project Changes

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -13,18 +13,20 @@ public class TestRunner {
 	public void Test() {
 		RestAssured.baseURI = "https://reqres.in/";
 		RequestSpecification request = RestAssured.given();
-		
+//Get Request
 		given().
 		param("page", "2").
 		when().get("api/users").then().assertThat().statusCode(200);
-		
-		
+
+
 		String status = given().
 				param("page", "2").
 				when().get("api/users").body().asString();
 		System.out.println(status);
 
-		
+
+
+
 		JSONObject requestParam = new JSONObject();
 		requestParam.put("name", "Shakt");
 		requestParam.put("Job", "MD");
@@ -36,5 +38,3 @@ public class TestRunner {
 
 	}
 }
-
-
