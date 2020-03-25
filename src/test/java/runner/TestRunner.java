c1dc0d5f154d0f8d281f34882e40af318acdff5b
email <shakti.agrawal@publicissapient.com>
2020-03-25 19:37:08 +0530

    Rest Assured Demo - Commit 1

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,40 @@
+package runner;
+
+import org.json.simple.JSONObject;
+import org.junit.Test;
+import static io.restassured.RestAssured.given;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class TestRunner {
+	@Test
+	public void Test() {
+		RestAssured.baseURI = "https://reqres.in/";
+		RequestSpecification request = RestAssured.given();
+		
+		given().
+		param("page", "2").
+		when().get("api/users").then().assertThat().statusCode(200);
+		
+		
+		String status = given().
+				param("page", "2").
+				when().get("api/users").body().asString();
+		System.out.println(status);
+
+		
+		JSONObject requestParam = new JSONObject();
+		requestParam.put("name", "Shakt");
+		requestParam.put("Job", "MD");
+		request.body(requestParam.toJSONString());
+		Response response = request.post("api/users");
+		int statusCode = response.getStatusCode();
+		System.out.println(statusCode);
+		System.out.println(response.body().asString());
+
+	}
+}
+
+
